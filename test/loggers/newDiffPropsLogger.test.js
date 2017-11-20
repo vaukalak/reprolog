c9ff6e85a8f428ed81ffdbb9d755cca580dcce3d
vaukalak <lahmataja.pa4vara@gmail.com>
2017-11-20 13:09:07 +0300

    version 0.1.6, improved loggers and outputs importing, fixed some bugs

--- test/loggers/newDiffPropsLogger.test.js
+++ test/loggers/newDiffPropsLogger.test.js
@@ -1,10 +1,10 @@
-import newPropsLogger from '../../lib/loggers/newDiffPropsLogger';
+import newDiffPropsLogger from '../../lib/loggers/newDiffPropsLogger';
 
 describe('newDiffPropsLogger', () => {
   beforeEach(() => {
     const a = { foo: 1, baz: 2 };
 
-    newPropsLogger({
+    newDiffPropsLogger({
       logInit: (name, groups) => {
         expect(name).toEqual('temp');
         expect(groups).toEqual([{ name: 'initial props', value: a }]);
@@ -14,7 +14,7 @@ describe('newDiffPropsLogger', () => {
 
   it('should output all diffs correctly', () => {
     const a = { foo: 2, bar: 1 };
-    newPropsLogger({
+    newDiffPropsLogger({
       logUpdate: (name, groups) => {
         expect(name).toEqual('temp');
         expect(groups).toEqual([
@@ -22,6 +22,21 @@ describe('newDiffPropsLogger', () => {
           { name: 'additions', value: { bar: 1 } },
           { name: 'deletions', value: { baz: 2 } },
           { name: 'changes', value: { foo: 2 } },
+        ]);
+      },
+    }).logUpdate('temp', a);
+  });
+
+  it('should not consider `undefined` as deletion', () => {
+    const a = { baz: undefined };
+    newDiffPropsLogger({
+      logUpdate: (name, groups) => {
+        expect(name).toEqual('temp');
+        expect(groups).toEqual([
+          { name: 'new props', value: a },
+          { name: 'additions', value: { } },
+          { name: 'deletions', value: { foo: 1 } },
+          { name: 'changes', value: { baz: undefined } },
         ]);
       },
     }).logUpdate('temp', a);
